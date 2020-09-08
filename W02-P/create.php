19f120d3f620a2db1476fa7ec48bababb5fd4b7b
una0106 <44970801+una0106@users.noreply.github.com>
2020-09-08 12:56:31 +0900

    Update create.php

--- W02-P/create.php
+++ W02-P/create.php
@@ -9,7 +9,10 @@
 
   $article = array(
     'title' => 'Welcome',
-    'description' => 'Database is ...'
+    'description' => 'The Minions are small, yellow, cylindrical creatures, who have one or two eyes. They are the signature characters of the Despicable Me series. They bring much of the comedy in the film, and they are known as the scene-stealer of the movie. Frequently, they speak in an incomprehensible language, called Minionese, occasionally switching to English. They are much childish in some ways, yet they seem to be very intelligent in certain aspects. All the minions are minor characters in Minions (with the exception of Kevin, Stuart, and Bob), major character in Despicable Me and Despicable Me 2 and supporting characters in Despicable Me 3 (with the exception of Mel).
+Jerry, Kevin, Stuart, Dave and Carl having an ice cream party from Despicable Me 2
+They are impulsive creatures with little self-control, but with a wide-eyed wonder and odd innocence that endears them to viewers and makes them relatable. They can be pesky when they are doing weird interactions with other people, animals, or objects; they are also famous for their gibberish-speaking language. Minions have standard English names (see below).
+Unlike most other criminal masterminds and their usual doctrine of abusing their henchmen, Gru gets along famously with the minions. He genuinely seems to like them and shows appreciation for their hard work and support, only having to be strict with them very few times. He even seems to know each of them by name.'
   );
 
   if( isset($_GET['id'])) {
